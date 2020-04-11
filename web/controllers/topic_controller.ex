c83343164c8f59e0b173282e6d3b00513b24d314
Stephen Gladney <stephen@ordernerd.com>
2020-04-11 12:17:53 -0400

    New topic page is loading

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,16 @@
+defmodule Discuss.TopicController do
+    use Discuss.Web, :controller
+    alias Discuss.Topic
+
+    def new(conn, params) do
+        changeset = Topic.changeset(%Topic{}, %{})
+
+        render conn, "new.html", changeset: changeset
+    end
+
+    def create(conn, params) do
+        %{"topic" => topic} = params
+    end
+
+    
+end
