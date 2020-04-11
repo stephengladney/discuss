da2d7c883e29625ab32ab60bc9d2420b20acf89b
Stephen Gladney <stephen@ordernerd.com>
2020-04-11 15:05:51 -0400

    Add Ecto.create to create method

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,14 +2,19 @@ defmodule Discuss.TopicController do
     use Discuss.Web, :controller
     alias Discuss.Topic
 
-    def new(conn, params) do
+    def new(conn, _params) do
         changeset = Topic.changeset(%Topic{}, %{})
 
         render conn, "new.html", changeset: changeset
     end
 
-    def create(conn, params) do
-        %{"topic" => topic} = params
+    def create(conn, %{"topic" => topic}) do
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repo.insert(changeset) do
+            {:ok, post} -> IO.inspect(post)
+            {:error, changeset} -> IO.inspect(changeset) 
+        end
     end
 
     
