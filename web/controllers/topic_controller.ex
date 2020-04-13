1414b5d1e5204949b9c6792498a12993077e79de
Stephen Gladney <stephen@ordernerd.com>
2020-04-13 00:40:58 -0400

    Add flash messages and redirect

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,18 +2,28 @@ defmodule Discuss.TopicController do
     use Discuss.Web, :controller
     alias Discuss.Topic
 
-    def new(conn, _params) do
-        changeset = Topic.changeset(%Topic{}, %{})
+    def index(conn, _params) do
+        topics = Repo.all(Topic) |> Enum.reverse()
+        render conn, "index.html", topics: topics
+    end
 
-        render conn, "new.html", changeset: changeset
+    def new(conn, _params) do
+        render conn, "new.html", changeset: Topic.changeset(%Topic{}, %{})
     end
 
     def create(conn, %{"topic" => topic}) do
         changeset = Topic.changeset(%Topic{}, topic)
 
         case Repo.insert(changeset) do
-            {:ok, post} -> IO.inspect(post)
-            {:error, changeset} -> IO.inspect(changeset) 
+            {:ok, post} -> 
+                conn
+                |> put_flash(:info, "Topic created!")
+                |> redirect(to: topic_path(conn, :index))
+                # render conn, "index.html", topics: Repo.all(Topic)
+            {:error, changeset} ->
+                conn
+                |> put_flash(:error, "There was an error creating the topic.") 
+                render "new.html", changeset: changeset
         end
     end
 
